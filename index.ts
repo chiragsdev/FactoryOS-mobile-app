bb94439d3f867e183dd7d32e61091e3bf673aa55
chiragzeus <chirags@zeusint.com>
2026-06-29 12:28:20 +0530

    fix: DOMException polyfill for Hermes

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
+if (typeof (globalThis as any).DOMException === "undefined") {
+  (globalThis as any).DOMException = class DOMException extends Error {
+    constructor(message?: string, name?: string) {
+      super(message);
+      this.name = name || "DOMException";
+    }
+  };
+}
+
+import { registerRootComponent } from "expo";
+import App from "./App";
+
 registerRootComponent(App);
